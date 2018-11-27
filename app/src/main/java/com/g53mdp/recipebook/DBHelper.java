74950a905e45c3449322130118adc5a61bba3b25
csnand <askmryang@outlook.com>
2018-11-27 16:17:19 +0000

    getting started

---
+++ app/src/main/java/com/g53mdp/recipebook/DBHelper.java
@@ -0,0 +1,30 @@
+package com.g53mdp.recipebook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context, String name, CursorFactory factory, int version){
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE recipeList ( id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255), content VARCHAR(255), rating VARCHAR(64) ); ");
+
+        db.execSQL( " INSERT INTO recipeList (title, content, rating) VALUES ('test Recipe One', 'test Content One', '10') " );
+        db.execSQL( " INSERT INTO recipeList (title, content, rating) VALUES ('test Recipe Two', 'test Content Two', '9') " );
+        db.execSQL( " INSERT INTO recipeList (title, content, rating) VALUES ('test Recipe Three', 'test Content Three', '8') " );
+        db.execSQL( " INSERT INTO recipeList (title, content, rating) VALUES ('test Recipe Four', 'test Content Four', '7') " );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(" DROP TABLE IF EXISTS recipaList ");
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/g53mdp/recipebook/RecipeContentProvider.java
@@ -0,0 +1,51 @@
+package com.g53mdp.recipebook;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+
+public class RecipeContentProvider extends ContentProvider {
+
+    private DBHelper dbHelper = null;
+
+
+    @Override
+    public boolean onCreate() {
+
+        this.dbHelper = new DBHelper(this.getContext(), "recipeContentProvicer", null, 1);
+        return true;
+    }
+
+    
+    @Override
+    public Cursor query( Uri uri,  String[] projection,  String selection,  String[] selectionArgs,  String sortOrder) {
+        return null;
+    }
+
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    
+    @Override
+    public Uri insert( Uri uri,  ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete( Uri uri,  String selection,  String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update( Uri uri,  ContentValues values,  String selection,  String[] selectionArgs) {
+        return 0;
+    }
+
+
+}
